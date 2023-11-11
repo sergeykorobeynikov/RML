8e6282da7bef6a85cc99c2e83e43a005f0d6a1a3
sergeykorobeynikov <ksn@ksn.name>
2023-11-11 12:39:39 +0600

    Добавьте файлы проекта.

---
+++ RML.ExtraModule/BusinessObjects/Country.cs
@@ -0,0 +1,20 @@
+﻿using DevExpress.Persistent.Base;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RML.ExtraModule.BusinessObjects
+{
+    [DefaultClassOptions]
+    [NavigationItem("Core")]
+    [Table("Countries", Schema = "core")]
+    [Microsoft.EntityFrameworkCore.Index(nameof(Code2), IsUnique = true)]
+    [Microsoft.EntityFrameworkCore.Index(nameof(Code3), IsUnique = true)]
+    public class Country
+    {
+        [Browsable(false)]
+        public virtual int Id { get; set; }
+        public virtual string Code2 { get; set; }
+        public virtual string Code3 { get; set; }
+        public virtual string Name { get; set; }
+    }
+}
